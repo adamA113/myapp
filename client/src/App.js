e6b6aed3999ffe6b44d7b28fc64ae125dc23f455
Yasir Tobbileh <yasur96@gmail.com>
2021-05-25 21:00:12 +0300

    feat/ configuring the server, mongoDB, and add graphql and cloudinary settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,31 @@
 import './App.css';
 import React, { Component } from 'react';
-import Navbar from './Navbar';
-import Formphoto from './Formphoto';
-import Gallery from './Gallery';
-import Photo from './Photo';
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false,
-      photos: [],
-      label: null,
-      url: null,
-      password: null
-    };
-    console.log(this.state);
-  }
+// import Navbar from './Navbar';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+// import Formphoto from './Formphoto';
+import UploadPhoto from './components/UploadPhoto/UploadPhoto';
+import Gallery from './components/Gallery';
+// import Photo from './Photo';
 
-  addPhoto = (photo) => {
-    let photos = [...this.state.photos, photo];
-    this.setState({
-      photos: photos
-    });
-  }
-  handleCallback = (Data) => {
-    this.setState({ show: Data })
-  }
-  handleCallback1 = (data) => {
-    this.setState({ label: data.label, url: data.url, password: data.password })
-    // console.log(data.url);
-  }
-  render() {
-    return (
-      <div className="main">
-        <Navbar callback={this.handleCallback}/>
-        <Formphoto handleAdd={this.state.show} callback={this.handleCallback} callback1={this.handleCallback1} addPhoto={this.addPhoto}/>
-        <Photo photo={this.state}/>
-        {/* <Gallery photos={this.state.photos}/> */}
+const client = new ApolloClient({
+	uri: 'http://localhost:3001/graphql',
+	cache: new InMemoryCache()
+});
 
-      </div>
-    );
-  }
+function App() {
+	return (
+		<ApolloProvider client={client}>
+			<div className="main">
+				{/* <Navbar callback={this.handleCallback} />
+          <Formphoto handleAdd={this.state.show} callback={this.handleCallback} callback1={this.handleCallback1} addPhoto={this.addPhoto} />
+          <Photo photo={this.state} /> */}
+				<UploadPhoto />
+				<Gallery />
+			</div>
+		</ApolloProvider>
+
+	);
 }
 
 export default App;
