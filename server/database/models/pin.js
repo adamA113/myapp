4bd801a63140e567a2d9858d1ada6781864a6789
Yasir Tobbileh <yasur96@gmail.com>
2021-05-25 20:54:19 +0300

    feat/ create pin model

---
+++ server/database/models/pin.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const pinSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+const Pin = mongoose.model('Pin', pinSchema);
+
+module.exports = Pin;
