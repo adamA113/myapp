cc871d32b3fda5bad9bf856125649162cc655117
Yasir Tobbileh <yasur96@gmail.com>
2021-06-09 14:15:53 +0300

    feat/ add delete pin mutation, and refactor some queries

--- server/graphql/resolvers/pins.js
+++ server/graphql/resolvers/pins.js
@@ -1,48 +1,86 @@
+const { AuthenticationError } = require('apollo-server');
 require('dotenv').config();
 
 const { cloudinary } = require('../../utils/cloudinary');
 const Pin = require('../../database/models/Pin');
+const checkAuth = require('../../utils/checkAuth');
 
 const pinsResolvers = {
     Query: {
-        pin: (parent, args) => {
-            console.log(parent);
-            return Pin.findById(args.id)
+        pin: async (parent, { id }) => {
+            try {
+                const post = await Pin.findById(id);
+                if (post) {
+                    return post;
+                }
+                throw new Error("Post not found");
+            }
+            catch (err) {
+                throw new Error(err);
+            }
         },
-        pins: (parent, args) => {
-            return Pin.find({})
+        pins: async (parent, args, context) => {
+            // console.log("=====", context.req.headers);
+            try {
+                return await Pin.find({}).sort({ createdAt: -1 });
+            }
+            catch (err) {
+                throw new Error(err);
+            }
         },
     },
-    
+
     Pin: {
         user: (parent) => {
-            return User.findById(parent.userId)
+            return User.findById(parent.userId);
         }
     },
 
     Mutation: {
-        addPin: async (parent, { title, description, imageURL, userId }) => {
-            console.log(userId);
+        addPin: async (parent, { title, description, imageURL }, context) => {
+            // console.log(context.req.headers);
             try {
-                const fileStr = imageURL;
-                // console.log("=====", fileStr)
-                const uploadedResponse = await cloudinary.uploader.upload(fileStr,
-                    { upload_preset: 'pinterest' }, function (error, result) {
-                        console.log(result);
-                    })
+                const user = checkAuth(context);
+                // console.log("user", user);
+                // const fileStr = imageURL;
+                // const uploadedResponse = await cloudinary.uploader.upload(fileStr,
+                //     { upload_preset: 'pinterest' }, function (error, result) {
+                //         console.log(result);
+                //     })
                 // res.send(uploadedResponse.public_id)
                 let pin = new Pin({
                     title,
                     description,
-                    imageId: uploadedResponse.public_id,
-                    userId
+                    imageId: imageURL,
+                    // imageId: uploadedResponse.public_id,
+                    userId: user.id
 
                 });
                 return pin.save();
             }
             catch (err) {
-                console.log(err);
+                throw new Error(err);
             }
+        },
+
+        deletePin: async (parent, { id }, context) => {
+            const user = checkAuth(context);
+
+            try {
+                const pin = await Pin.findById(id);
+                if(user.id === pin.userId){
+                    await pin.delete();
+                    return "Pin has been successfully deleted";
+                }
+                else{
+                    throw new AuthenticationError("You are not authorized to delete this pin");
+                }
+            }
+            catch (err) {
+                throw new Error(err);
+
+            }
+
         }
     }
 }
